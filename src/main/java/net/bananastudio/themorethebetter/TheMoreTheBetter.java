611639318e024747eda6c10fba0ff50803074b6a
bananastudio <michiel1.debacker@gmail.com>
2022-08-10 15:23:16 +0200

    COMMITTTTTTTTTTTTTTTTTTTTT

--- src/main/java/net/bananastudio/themorethebetter/TheMoreTheBetter.java
+++ src/main/java/net/bananastudio/themorethebetter/TheMoreTheBetter.java
@@ -1,5 +1,6 @@
 package net.bananastudio.themorethebetter;
 
+import net.bananastudio.themorethebetter.block.ModBlocks;
 import net.bananastudio.themorethebetter.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -12,5 +13,6 @@ public class TheMoreTheBetter implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
